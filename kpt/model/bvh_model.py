Fix rotation issue

kpt/model/bvh_model.py
@@ -5,6 +5,7 @@ import torch
 from kpt.model.kinematic_model import KinematicModel
 from pymo.parsers import BVHParser
 from pytorch3d.transforms import euler_angles_to_matrix
+from scipy.spatial.transform import Rotation
 
 
 class BVHModel(KinematicModel):
@@ -54,8 +55,12 @@ class BVHModel(KinematicModel):
         root_position = torch.Tensor(np.expand_dims(root_position_sr.values, axis=0)).T
 
         root_global_rotation_sr = cur_frame[[self.root_name + '_' + rot for rot in ['Zrotation', 'Yrotation', 'Xrotation']]]
-        root_rotation = euler_angles_to_matrix(torch.Tensor(root_global_rotation_sr.values), channel_order)
+        # root_rotation = euler_angles_to_matrix(torch.Tensor(root_global_rotation_sr.values), channel_order)
 
+        z_rot = Rotation.from_euler('z', root_global_rotation_sr[0], degrees=True).as_matrix()
+        y_rot = Rotation.from_euler('y', root_global_rotation_sr[1], degrees=True).as_matrix()
+        x_rot = Rotation.from_euler('x', root_global_rotation_sr[2], degrees=True).as_matrix()
+        root_rotation = torch.Tensor(z_rot @ y_rot @ x_rot)
         return root_position, root_rotation
     
     def get_rotation_matrix(self, frame_id: int, joint_name: str, channel_order: str):
@@ -65,6 +70,11 @@ class BVHModel(KinematicModel):
             return torch.eye(3)
 
         rot_cols = [joint_name + '_' + channel for channel in ['Zrotation', 'Yrotation', 'Xrotation']]
-        rot_mat = euler_angles_to_matrix(torch.Tensor(cur_frame[rot_cols].values/180*np.pi), channel_order) # TODO: find better way of converting radian
+        # rot_mat = euler_angles_to_matrix(torch.Tensor(cur_frame[rot_cols].values/180*np.pi), channel_order) # TODO: find better way of converting radian
+
+        z_rot = Rotation.from_euler('z', cur_frame[rot_cols].values[0], degrees=True).as_matrix()
+        y_rot = Rotation.from_euler('y', cur_frame[rot_cols].values[1], degrees=True).as_matrix()
+        x_rot = Rotation.from_euler('x', cur_frame[rot_cols].values[2], degrees=True).as_matrix()
+        rot_mat = torch.Tensor(z_rot @ y_rot @ x_rot)
         return rot_mat
 

kpt/model/robot_model.py
@@ -22,7 +22,7 @@ class RobotModel:
             frame_id (int): [description]
         """
         self.frame_id = frame_id
-        if self.kinematic_model.model_type is 'bvh':
+        if self.kinematic_model.model_type == 'bvh':
            self._kinematic_chain = self.kinematic_model.get_kinematic_chain()
            channel_order = self._kinematic_chain[self.kinematic_model.root_name]['channel_order']
            root_pos, root_rot = self.kinematic_model.get_root_pos_rot(frame_id, channel_order)
@@ -30,7 +30,7 @@ class RobotModel:
             raise ValueError('Undefined model type')
 
         self._kinematic_chain[self.kinematic_model.root_name]['p'] = root_pos
-        self._kinematic_chain[self.kinematic_model.root_name]['R'] = torch.eye(3)
+        self._kinematic_chain[self.kinematic_model.root_name]['R'] = root_rot
         
     
     def forward_kinematics(self, joint_name):

tests/test_experiment.py
@@ -1,7 +1,12 @@
 # This is for testing/debugging third party libraries
 
-from pymo.preprocessing import MocapParameterizer
+import numpy as np
+import torch
 from pymo.parsers import BVHParser
+from pymo.preprocessing import MocapParameterizer
+from pytorch3d.transforms import euler_angles_to_matrix
+from scipy.spatial.transform import Rotation
+
 
 def test_pymo_parameterizer():
     path = 'sample_data/bvh/02_03.bvh' # Short motion ;)
@@ -11,3 +16,18 @@ def test_pymo_parameterizer():
     position_df_cols = positions[0].values.columns.tolist()
     for col in position_df_cols:
         assert 'position' in col
+
+
+def test_euler_rotmat_transform():
+
+    rot_mat_x = Rotation.from_euler('x', 30, degrees=True).as_matrix()
+    rot_mat_y = Rotation.from_euler('y', 60, degrees=True).as_matrix()
+    rot_mat_z = Rotation.from_euler('z', 90, degrees=True).as_matrix()
+    
+    rot_mat1 = rot_mat_z @ rot_mat_y @ rot_mat_x
+
+    rot_euler = torch.Tensor(np.array([30, 60, 90])/(180)*np.pi)
+    
+    rot_mat2 = euler_angles_to_matrix(rot_euler, 'YZX')
+
+    print("done")

tests/test_vis.py
@@ -18,7 +18,7 @@ def test_scatter_animation():
     bvh_model = BVHModel(path)
     robot_model = RobotModel(bvh_model)
 
-    total_sequence = 1200
+    total_sequence = 1500
     position_arrs = []
     for i in range(total_sequence):
         robot_model.set_frame(i)
@@ -28,3 +28,11 @@ def test_scatter_animation():
     position_arrs = np.stack(position_arrs)
     scatter_animation(position_arrs, 'test_viz.mp4')
 
+def test_reference():
+    import pandas as pd
+    df = pd.read_csv('assets/cmu_01_01_pos.csv')
+    position_arrs = []
+    for i in range(len(df)):
+        position_arrs.append(df.iloc[i].values.reshape((-1,3)))
+    position_arrs = np.stack(position_arrs)
+    scatter_animation(position_arrs, 'reference_01_01.mp4')